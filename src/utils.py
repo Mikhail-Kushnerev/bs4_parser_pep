f497964eb2126e783d6c3c6ee4b581e106ff11cf
Mikhail-Kushnerev <mikushnerev@yandex.ru>
2022-07-26 15:20:16 +0300

    Ready for review

--- src/utils.py
+++ src/utils.py
@@ -18,16 +18,19 @@ def get_response(session, url):
 
 
 def find_tag(soup, tag=None, attrs=None, text=None):
-    if text:
-        searched_tag = soup.find(text=text)
+    try:
+        if text:
+            searched_tag = soup.find(text=text)
+        else:
+            searched_tag = soup.find(tag, attrs=(attrs or {}))
         if searched_tag is None:
+            raise
+    except:
+        if text:
             error_msg = f"Тэг, имеющий текст '{text}' не найден"
-            logging.error(error_msg, stack_info=True)
-            raise ParserFindTagException(error_msg)
-    else:
-        searched_tag = soup.find(tag, attrs=(attrs or {}))
-        if searched_tag is None:
+        else:
             error_msg = f"Не найден тег {tag} {attrs}"
-            logging.error(error_msg, stack_info=True)
-            raise ParserFindTagException(error_msg)
-    return searched_tag
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    else:
+        return searched_tag
